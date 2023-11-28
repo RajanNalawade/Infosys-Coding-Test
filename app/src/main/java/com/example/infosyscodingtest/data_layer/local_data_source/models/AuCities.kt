4b71b16be6bf292c5d78477ea36393c582377924
rajan.nalawade <raj.nalawade2337@gmail.com>
2023-11-28 14:44:27 +0530

    architectural setup done and added required dependecies

---
+++ app/src/main/java/com/example/infosyscodingtest/data_layer/local_data_source/models/AuCities.kt
@@ -0,0 +1,6 @@
+package com.example.infosyscodingtest.data_layer.local_data_source.models
+
+
+import com.google.gson.annotations.SerializedName
+
+class AuCities : ArrayList<AuCitiesItem>()
---
+++ app/src/main/java/com/example/infosyscodingtest/data_layer/local_data_source/models/AuCitiesItem.kt
@@ -0,0 +1,25 @@
+package com.example.infosyscodingtest.data_layer.local_data_source.models
+
+
+import com.google.gson.annotations.SerializedName
+
+data class AuCitiesItem(
+    @SerializedName("admin_name")
+    val adminName: String?,
+    @SerializedName("capital")
+    val capital: String?,
+    @SerializedName("city")
+    val city: String?,
+    @SerializedName("country")
+    val country: String?,
+    @SerializedName("iso2")
+    val iso2: String?,
+    @SerializedName("lat")
+    val lat: String?,
+    @SerializedName("lng")
+    val lng: String?,
+    @SerializedName("population")
+    val population: String?,
+    @SerializedName("population_proper")
+    val populationProper: String?
+)
